ca333a3413dd79c7e792aebc790513f2496bd0c3
sproshchaev <sproshchaev@gmail.com>
2024-07-03 20:14:09 +0500

    feat: add module w-05 w-06

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,3 +19,13 @@ include("webinar-03:producer-service")
 findProject(":webinar-03:producer-service")?.name = "producer-service"
 include("webinar-04:admin-service")
 findProject(":webinar-04:admin-service")?.name = "admin-service"
+include("webinar-05")
+include("webinar-06")
+include("webinar-05:producer-service")
+findProject(":webinar-05:producer-service")?.name = "producer-service"
+include("webinar-06:producer-service")
+findProject(":webinar-06:producer-service")?.name = "producer-service"
+include("webinar-05:consumer-service")
+findProject(":webinar-05:consumer-service")?.name = "consumer-service"
+include("webinar-06:stream-service")
+findProject(":webinar-06:stream-service")?.name = "stream-service"
---
+++ webinar-06/stream-service/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+}
+
+group = "com.prosoft"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation ("org.apache.kafka:kafka-streams:3.7.0")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+
+    implementation ("org.slf4j:slf4j-api:2.0.13")
+    implementation ("ch.qos.logback:logback-classic:1.5.6")
+
+    implementation("org.projectlombok:lombok:1.18.32")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
