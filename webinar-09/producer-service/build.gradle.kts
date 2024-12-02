303b43f9544ae5755c2f75ba8417c63e2aef5f82
sproshchaev <sproshchaev@gmail.com>
2024-12-02 18:55:36 +0500

    add webinar-09

---
+++ webinar-09/producer-service/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "com.prosoft"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.kafka:spring-kafka")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
